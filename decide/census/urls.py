9786f04696d1759e1197581597ac080eeafe9ad3
jullopjim1 <jullopjim1@alumn.us.es>
2018-12-12 20:07:31 +0100

    UPDATE: Creacion del CensoHe importado los metodos del views.py al urls.py de la carpeta CENSUS.# Por favor ingrese el mensaje del commit para sus cambios. Las líneas que comiencen# con '#' serán ignoradas, y un mensaje vacío aborta el commit.## En la rama EC1-FrontEnd# Tu rama está actualizada con 'origin/EC1-FrontEnd'.## Cambios a ser confirmados:#	modificado:     census/urls.py#

--- decide/census/urls.py
+++ decide/census/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path, include
-from . import views
+from .views import CensusCreate, CensusDetail
 
 urlpatterns = [
     path('', CensusCreate.as_view(), name='census_create'),
