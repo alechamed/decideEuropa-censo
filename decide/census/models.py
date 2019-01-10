06b86462d0abc57ed1e7bffea64929625dc67214
jesusmtorres97 <jesus.torres.mateo@gmail.com>
2019-01-10 18:13:20 +0100

    UPDATE: census/model.py Añadido el usuario para hacer la mejora del filtrado.

--- decide/census/models.py
+++ decide/census/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from jsonschema._validators import maxLength
 
 
 class Census(models.Model):
@@ -7,3 +8,13 @@ class Census(models.Model):
 
     class Meta:
         unique_together = (('voting_id', 'voter_id'),)
+
+class Usuario(models.Model):
+    first_name = models.CharField(max_length = 50)
+    last_name = models.CharField(max_length = 50)
+    email = models.CharField(max_length = 50) #Único
+    username = models.CharField(max_length = 50) #Único
+    password = models.CharField(max_length = 50)
+    is_staff = models.BooleanField(null=false) #True or false
+    is_active = models.BooleanField(null=false) #Activo o inactivo 
+    date_joined = models.DateField(null=false)
