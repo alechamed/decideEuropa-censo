f65648722b01ced637928abc69597fd448b92788
Alejandro <alechamed@alum.us.es>
2019-01-12 20:29:59 +0100

    Update: Fusion manual de ficheros DEV y EC4
    He copiado manualmente el DEV con los cambios de EC4.
    
    Refers #8

--- decide/decide/settings.py
+++ decide/decide/settings.py
@@ -43,10 +43,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework.authtoken',
     'rest_framework_swagger',
-    'import_export',
 ]
-
-IMPORT_EXPORT_USE_TRANSACTIONS = True
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
