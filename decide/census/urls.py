9a11e9fb03eac92fa8a77618dcd180914af44f25
jesusmtorres97 <jesus.torres.mateo@gmail.com>
2019-01-29 22:48:34 +0100

    UPDATE: views.py, urls.py, censusFilter.html, 3, 4 y 5; DELETE: censusFilter2.html Finalizado el filtro y borrados archivos sobrantes.

--- decide/census/urls.py
+++ decide/census/urls.py
@@ -12,7 +12,6 @@ urlpatterns = [
     path('list/<int:voting_id>/', CensusCreate.list),
     path('<int:voting_id>/', CensusDetail.as_view()),
     path('listUsers/', CensusFilter.listUsers),
-    path('listNombre/', CensusFilter.listNombre),
     path('listActivo/', CensusFilter.listActivos),
     path('listStaff/', CensusFilter.listStaff),
     path('listSuperuser/', CensusFilter.listSuperuser),
