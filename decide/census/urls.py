33121d4ea2795c9389329eff94f682b1e015b422
jesusmtorres97 <jesus.torres.mateo@gmail.com>
2019-01-30 18:48:24 +0100

    UPDATE: urls.py Había objetos duplicados y los test fallaban.

--- decide/census/urls.py
+++ decide/census/urls.py
@@ -1,11 +1,5 @@
 from django.urls import path, include
 from . import views
-
-urlpatterns = [
-    path('', views.CensusCreate.as_view(), name='census_create'),
-    path('<int:voting_id>/', views.CensusDetail.as_view(), name='census_detail')
-    ]
-
 from .views import CensusCreate, CensusDetail, CensusFilter
 
 urlpatterns = [
@@ -15,4 +9,6 @@ urlpatterns = [
     path('listActivo/', CensusFilter.listActivos),
     path('listStaff/', CensusFilter.listStaff),
     path('listSuperuser/', CensusFilter.listSuperuser),
+    path('', views.CensusCreate.as_view(), name='census_create'),
+    path('<int:voting_id>/', views.CensusDetail.as_view(), name='census_detail'),   
 ]
